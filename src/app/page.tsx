4c423e94425c5977f7b84da26c0139ab1551a32d
Oliver Gjorgjiev <oliver.gorgiev11@gmail.com>
2025-01-25 14:09:03 +0100

    fix padd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import KekiProgress from '@/components/keki-progress';
 
 export default function Home() {
   return (
-    <div className="flex lg:flex-row flex-col">
+    <div className="flex lg:flex-row px-8 gap-10 lg:px-20 lg:gap-20 flex-col">
       <ColeProgress /> <KekiProgress />
     </div>
   );
--- src/components/keki-progress.tsx
+++ src/components/keki-progress.tsx
@@ -60,7 +60,7 @@ export default function KekiProgress() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 lg:p-12 gap-8 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen py-2 gap-8 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-start max-w-full overflow-scroll">
         <h1 className="text-2xl sm:text-4xl font-bold">
           Keki&apos;s Daily habits
